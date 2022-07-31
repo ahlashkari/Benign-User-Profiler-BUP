a927bafbc46a197b4b76c9fdd03370b11d915ef9
Moein Shafi <mosafer.moein@gmail.com>
2022-08-01 02:55:40 +0430

    Complete VPN model.

--- BenignTrafficGenerator/traffic_models/vpn_model.py
+++ BenignTrafficGenerator/traffic_models/vpn_model.py
@@ -1,70 +1,59 @@
 #!/usr/bin/env python3
 
-import requests, json, sys, base64, tempfile, subprocess, time
+import base64
+import json
+import random
+import requests
+import subprocess
+import sys
+import tempfile
+import time
+from threading import Timer
 from .traffic_model import TrafficModel
 
 
-class TelnetModel(TrafficModel):
+class VPNModel(TrafficModel):
     def __init__(self, model_config: dict):
         self.__model_config = model_config
 
     def generate(self) -> None:
-        host = self.__model_config["host"]
-        username = self.__model_config["username"]
+        self.OPENVPN_PATH = "openvpn"
+        duration = self.__model_config["duration"]
+        username = self.__model_config["username"] if "username" in self.__model_config else None
         password = self.__model_config["password"] if "password" in self.__model_config else None
+        config_file = self.__model_config["config_file"] if "config_file" in self.__model_config else None
+        if config_file is not None:
+            self.connect(config_file, duration, username, password)
+        else:
+            self.connect_to_random_network(duration)
 
-
-        OPENVPN_PATH = "openvpn"
-        VPNGATE_API_URL = "http://www.vpngate.net/api/iphone/"
-        DEFAULT_COUNTRY = "US"
-        SELECTED_COUNTRY = ""
-        DEFAULT_SERVER = 0
-        YES = False
-
-        if len(sys.argv) > 1:
-            if sys.argv[1] == "-y":
-                YES = True
-            else:
-                SELECTED_COUNTRY = sys.argv[1]
-
+    def connect_to_random_network(self, duration):
+        self.VPNGATE_API_URL = "http://www.vpngate.net/api/iphone/"
         servers = []
         try:
             print("[-] Trying to get server's informations...")
-            servers = sorted(getServers(), key=lambda server: int(server["Score"]), reverse=True)
+            servers = sorted(self.getServers(), key=lambda server: int(server["Score"]), reverse=True)
         except:
             print("[!] Failed to get server's informations from vpngate.")
-            sys.exit(1)
+            return
 
         if not servers:
             print("[!] There is no running server on vpngate.")
-            sys.exit(1)
+            return
 
         print("[-] Got server's informations.")
 
-        countries = sorted(getCountries(servers))
-
-        if not SELECTED_COUNTRY:
-            printCountries(countries)
-
-        selected_country = selectCountry(countries)
-
-        print("[-] Gethering %s servers..." % (selected_country, ))
-
-        selected_servers = [server for server in servers if server['CountryShort'] == selected_country]
-        printServers(selected_servers)
-        selected_server = selectServer(selected_servers)
+        random_number = random.randint(0, len(servers) - 1)
+        selected_server = servers[random_number]
 
         print("[-] Generating .ovpn file of %s..." % (selected_server["IP"], ))
-
-        ovpn_path = saveOvpn(selected_server)
-
+        ovpn_path = self.saveOvpn(selected_server)
         print("[-] Connecting to %s..." % (selected_server["IP"], ))
+        self.connect(ovpn_path, duration)
 
-        connect(ovpn_path)
-
-    def getServers():
+    def getServers(self):
         servers = []
-        server_strings = requests.get(VPNGATE_API_URL).text
+        server_strings = requests.get(self.VPNGATE_API_URL).text
         for server_string in server_strings.replace("\r", "").split('\n')[2:-2]:
             (HostName, IP, Score, Ping, Speed, CountryLong, CountryShort, NumVpnSessions, Uptime, TotalUsers, TotalTraffic, LogType, Operator, Message, OpenVPN_ConfigData_Base64) = server_string.split(',')
             server = {
@@ -87,78 +76,7 @@ class TelnetModel(TrafficModel):
             servers.append(server)
         return servers
 
-    def getCountries(server):
-        return set((server['CountryShort'], server['CountryLong']) for server in servers)
-
-    def printCountries(countries):
-        print("    Connectable countries:")
-        newline = False
-        for country in countries:
-            print("    %-2s) %-25s" % (country[0], country[1])),
-            if newline:
-                print('\n'),
-            newline = not newline
-        if newline:
-            print('\n'),
-
-    def printServers(servers):
-        print("  Connectable Servers:")
-        for i in range(len(servers)):
-            server = servers[i]
-
-            ipreq = requests.get("https://ipinfo.io/%1s" % (server['IP']))
-            ipinfo = json.loads(ipreq.text)
-
-            print("    %2d) %-15s [%6.2f Mbps, ping:%4s ms, score: %3s, hostname: ," % (i,
-                                                                            server['IP'],
-                                                                            float(server['Speed'])/10**6,
-                                                                            server['Ping'],
-                                                                            server['Score']))#,
-                                                                            #                                                                        ipinfo['hostname']))
-
-            print("                          city: %1s, region: %2s, org: %3s ]\n" % (ipinfo['city'], ipinfo['region'], ipinfo['org'].split(' ', 1)[1]))
-
-    def selectCountry(countries):
-        selected = SELECTED_COUNTRY
-        default_country = DEFAULT_COUNTRY
-        short_countries = list(country[0] for country in countries)
-        if not default_country in short_countries:
-            default_country = short_countries[0]
-        if YES:
-            selected = default_country
-        while not selected:
-            try:
-                selected = input("[?] Select server's country to connect [%s]: " % (default_country, )).strip().upper()
-            except:
-                print("[!] Please enter short name of the country.")
-                selected = ""
-            if selected == "":
-                selected = default_country
-            elif not selected in short_countries:
-                print("[!] Please enter short name of the country.")
-                selected = ""
-        return selected
-
-    def selectServer(servers):
-        selected = -1
-        default_server = DEFAULT_SERVER
-        if YES:
-            selected = default_server
-        while selected == -1:
-            try:
-                selected = input("[?] Select server's number to connect [%d]: " % (default_server, )).strip()
-            except:
-                print("[!] Please enter vaild server's number.")
-                selected = -1
-            if selected == "":
-                selected = default_server
-    #        elif not selected.isdigit() or int(selected) >= len(servers):
-            elif  int(selected) >= len(servers):
-                print("[!] Please enter vaild server's number.")
-                selected = -1
-        return servers[int(selected)]
-
-    def saveOvpn(server):
+    def saveOvpn(self, server):
         _, ovpn_path = tempfile.mkstemp()
         ovpn = open(ovpn_path, 'w')
         ovpn.write(str(base64.b64decode(server["OpenVPN_ConfigData_Base64"])))
@@ -166,17 +84,19 @@ class TelnetModel(TrafficModel):
         ovpn.close()
         return ovpn_path
 
-    def connect(ovpn_path):
-        openvpn_process = subprocess.Popen(['sudo', OPENVPN_PATH, '--config', ovpn_path])
+    def connect(self, ovpn_path, duration, username=None, password=None):
+        command = f'sudo {self.OPENVPN_PATH} --config {ovpn_path}'
+        if username is not None and password is not None:
+            command += ' --auth-user-pass <(echo -e "{username}\\n{password}")'
+        elif password is not None:
+            command += ' --auth-user-pass <(echo -e "{username}")'
+
+        openvpn_process = subprocess.Popen(['bash', command], shell=True)
+
+        print(f"Running the VPN for {duration} seconds...")
+        timer = Timer(duration, openvpn_process.kill)
         try:
-            while True:
-                time.sleep(600)
-        # termination with Ctrl+C
-        except:
-            try:
-                openvpn_process.kill()
-            except:
-                pass
-            while openvpn_process.poll() != 0:
-                time.sleep(1)
-            print("[=] Disconnected OpenVPN.")
+            timer.start()
+            stdout, stderr = openvpn_process.communicate()
+        finally:
+            timer.cancel()

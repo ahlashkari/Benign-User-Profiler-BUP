f4b9e8f1d5c8e1a73f6e25f0495634d4be991744
Arash Habibi Lashkari <a.habibi.l@gmail.com>
2024-04-01 16:03:48 -0400

    removed the old version.

--- BenignUserProfiler/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-
-from . import traffic_models
-from .benign_user_profiler import BenignUserProfiler
-from .traffic_generator import TrafficGenerator
-from .config_loader import ConfigLoader
-from .scheduler import Scheduler
--- BenignUserProfiler/traffic_models/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-from .model_factory import ModelFactory
-from .traffic_model import TrafficModel
-from .http_model import HTTPModel
-from .ssh_model import SSHModel
-from .cmd_model import CMDModel
-from .email_model import SMTPModel, IMAPModel
-from .ftp_model import FTPModel
